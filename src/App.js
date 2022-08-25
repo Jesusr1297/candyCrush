6bbaf997bc0a82d79d258b4b600bf86c4cb87552
jesusr1297 <jesusr1297@outlook.com>
2022-08-24 20:39:55 -0500

    added check column of 3 function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import {useEffect, useState} from "react";
+
+
 const width = 8
 const candyColors = [
     'blue',
@@ -8,20 +11,49 @@ const candyColors = [
     'yellow',
 ]
 
-const App = () => {
+const App = (effect, deps) => {
+    const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+    const checkForColumnOfThree = () => {
+        for (let i = 0; i < 47; i++) {
+            const columnOfThree = [i, i + width, i + width * 2]
+            const decidedColor = currentColorArrangement[i]
+            if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+                columnOfThree.forEach(square => currentColorArrangement[square] = '')
+            }
+        }
+    }
     const createBoard = () => {
-         const randomColorArrangement = []
+        const randomColorArrangement = []
         for (let i = 0; i < width * width; i++) {
             const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
             randomColorArrangement.push(randomColor)
         }
-        console.log(randomColorArrangement)
+        setCurrentColorArrangement(randomColorArrangement)
     }
 
-    createBoard()
+    useEffect(() => {
+        createBoard()
+    }, [])
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            checkForColumnOfThree()
+            setCurrentColorArrangement([...currentColorArrangement])
+        }, 100)
+        return () => clearInterval(timer)
+    }, [checkForColumnOfThree], currentColorArrangement)
 
     return (
-        <div>
+        <div className='app'>
+            <div className={'game'}>
+                {currentColorArrangement.map((candyColor, index) => (
+                        <img
+                            key={index}
+                            style={{backgroundColor: candyColor}}
+                        />
+                    )
+                )}
+            </div>
         </div>
     )
 }
